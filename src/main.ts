f70c4e85832a5acad443a1b10489e60f0fe81247
Aman Garg <aman.garg@blucursor.com>
2025-12-02 14:36:38 +0530

    Cors enabled for all origins

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: process.env.Frontend_URL,
-    credentials: true,
-  });
+  app.enableCors();
   app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3000);
 }
